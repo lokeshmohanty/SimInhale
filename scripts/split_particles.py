087006ad48416a38e67a03db146cc58d3b498799
Lokesh Mohanty <lokesh1197@gmail.com>
2023-08-17 15:00:25 +0530

    split_particles.py: add a all-zeros row to empty deposition dataframes

--- scripts/split_particles.py
+++ scripts/split_particles.py
@@ -4,10 +4,7 @@
 import os
 import sys
 import pandas as pd
-
-# if len(sys.argv) == 1:
-#     print("please pass the number of timesteps as argument")
-#     quit()
+from tqdm import tqdm
 
 path = sys.argv[1] or "."
 deposited_path = os.path.join(path, "deposited")
@@ -19,20 +16,22 @@ siminhale_files = [file for file in all_files if file.startswith("siminhale_") a
 
 N = len(siminhale_files)
 print("Number of files: ", N)
-# N = int(sys.argv[2] or len(siminhale_files))
+print("Deposited path: ", deposited_path)
+print("Not deposited path: ", not_deposited_path)
 
 if not os.path.exists(deposited_path):
     os.makedirs(deposited_path)
     os.makedirs(not_deposited_path)
 
-for filename in siminhale_files:
-# for i in range(1, N, 1):
-#     filename = f"siminhale_{i:04d}.csv"
-#     if filename in siminhale_files:
+for i in tqdm(range(len(siminhale_files))):
+    filename = siminhale_files[i]
     index = filename.split("_")[1].split(".")[0]
     df = pd.read_csv(os.path.join(path, filename))
     df_deposited = df[df["deposition"] == 1]
     df_not_deposited = df[df["deposition"] != 1]
+
+    if df_deposited.empty:
+       df_deposited[0, :] = [0 for _ in range(len(df.columns))] 
 
     df_deposited.to_csv(os.path.join(deposited_path, f"siminhale_deposited_{index}.csv"), index=False)
     df_not_deposited.to_csv(os.path.join(not_deposited_path, f"siminhale_not_deposited_{index}.csv"), index=False)
